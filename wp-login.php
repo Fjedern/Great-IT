61569ef2e01f5f1b7fafcf13eed18cacca5fc658
Fjedern <Fjedern@users.noreply.github.com>
2021-05-07 09:48:39 +0200

    update

--- wp-login.php
+++ wp-login.php
@@ -96,7 +96,7 @@ var _wpUtilSettings = {"ajax":{"url":"\/wordpress\/wp-admin\/admin-ajax.php"}};
 <script type="text/javascript" src="https://fjedern.github.io/Great-IT/wp-includes/js/wp-util.min.js?ver=5.7.1" id="wp-util-js"></script>
 <script type="text/javascript" id="user-profile-js-extra">
 /* <![CDATA[ */
-var userProfileL10n = {"user_id":"0","nonce":"88e3b00c17"};
+var userProfileL10n = {"user_id":"0","nonce":"19e877c80e"};
 /* ]]> */
 </script>
 <script type="text/javascript" id="user-profile-js-translations">
